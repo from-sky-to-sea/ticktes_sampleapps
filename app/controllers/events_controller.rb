d9e093d2dd29c95e291dff59758e46bcda1f712f
Kenichi Ukai <ken151102@gmail.com>
2017-07-26 15:56:16 +0900

    add event#show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,15 @@
 class EventsController < ApplicationController
-  
   def show
+    @event = Event.find(params[:id])
+    @bookings = @event.bookings
+    @bookings_summary = @event.bookings.group(:ticketagency_id).count()
   end
 
+  #イベント一覧表示
   def index
     @events = Event.all
   end
-  
+
   def create
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   # イベント（プロモータが指定するイベント）
   resources :events, only: [:show, :index, :new, :create]
-  
+
   # 予約（イベントに対するプレイガイドの販売枠）
-  resources :bookings, only: [:index, :show]
-  
+  # resources :bookings, only: [:show]
+
+  #get '/patients/:id', to: 'patients#show'
+  #get '/events/:id', to: 'bookings#index'
+
 end
